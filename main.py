bf26c65cfe638254ad5dd8f1531a0cc5d7335c70
Lucas Neto <lucasn@animaeducacao.com.br>
2022-10-04 15:41:11 -0300

    init2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-app.run()
+app.run(port=os.getenv('PORT'))
