6b271f18c5bd09b8d26af4db73050fb593e23f51
Lucas Neto <61164638+lucasnneto@users.noreply.github.com>
2022-12-17 21:40:18 -0300

    fix2

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return jsonify('msg':"Deu certo")
+    return jsonify({'msg':"Deu certo"})
 
 app.run(host='0.0.0.0',port=os.getenv('PORT'))
